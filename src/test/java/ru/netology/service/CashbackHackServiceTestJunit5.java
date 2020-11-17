c98f448f21829b8ef30d059f138e87e76a7f494b
Petr Filatov <pmfilatov@gmail.com>
2020-11-17 15:56:35 +0300

    fixed JUnit5 test

--- src/test/java/ru/netology/service/CashbackHackServiceTestJunit5.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTestJunit5.java
@@ -3,30 +3,29 @@ package ru.netology.service;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.*;
 
-class CashbackHackServiceTestJunit5 {
+public class CashbackHackServiceTestJunit5 {
 
     @Test
-    public void amountLess2000() {
+    public void amountLess1000() {
         CashbackHackService service = new CashbackHackService();
-        long actual = service.remain(1900);
+        long actual = service.remain(900);
         long expected = 100;
         assertEquals(expected, actual);
     }
 
     @Test
-    public void amountEquals2000() {
+    public void amountEquals1000() {
         CashbackHackService service = new CashbackHackService();
-        long actual = service.remain(2000);
+        long actual = service.remain(1000);
         long expected = 0;
         assertEquals(expected, actual);
     }
 
     @Test
-    public void amountOver2000() {
+    public void amountOver1000() {
         CashbackHackService service = new CashbackHackService();
-        long actual = service.remain(2101);
+        long actual = service.remain(1101);
         long expected = 899;
         assertEquals(expected, actual);
     }
